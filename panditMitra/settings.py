3e56d4706b3d6061db5263b3133744a2606f097c
Naman Jain <81414465+naman1852@users.noreply.github.com>
2024-03-01 20:02:54 +0530

    Update settings.py

--- panditMitra/settings.py
+++ panditMitra/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&@@xe6a+0br6gydti3gzx&a$kq#!70kxjsrgvmu*85(+)ja+$=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
